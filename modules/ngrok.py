ddc28959399977c2b9f9f1a45474d991d05ebeb3
dr12ak <148722760+dr12ak@users.noreply.github.com>
2024-01-24 19:22:21 +0100

    Update ngrok.py

--- modules/ngrok.py
+++ modules/ngrok.py
@@ -12,8 +12,6 @@ def connect(token, port, options, domain=None):
             account = f"{username}:{password}"
     
     # For all options see: https://github.com/ngrok/ngrok-py/blob/main/examples/ngrok-connect-full.py
-    if domain is not None:
-        options['domain'] = domain
     if not options.get('authtoken_from_env'):
         options['authtoken'] = token
     if account:
